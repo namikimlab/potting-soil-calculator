f2761772fadcaf042fbecefbb6c2217f09c6c518
namikimlab <namixkim@gmail.com>
2025-05-20 20:47:13 +0900

    Adjust button size

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,15 +5,19 @@ type ProductCardProps = {
 };
 
 const ProductCard = ({ imageSrc, alt, href }: ProductCardProps) => (
-  <div className="flex flex-col items-center space-y-1">
+  <div className="w-24 flex flex-col items-center space-y-1">
     <a href={href} target="_blank" rel="noopener noreferrer">
-      <img src={imageSrc} alt={alt} className="w-24 h-24 object-cover rounded shadow" />
+      <img
+        src={imageSrc}
+        alt={alt}
+        className="w-24 h-24 object-cover rounded shadow"
+      />
     </a>
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-lg bg-green-600 text-white px-3 py-1 rounded"
+      className="text-lg bg-green-600 text-white px-3 py-1 rounded text-center w-full"
     >
       구매하기
     </a>
